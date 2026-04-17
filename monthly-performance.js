3eed73a85db9a5ed50bfb9599fa0783addde221b
tuofficedrive1-wq <tuofficedrive1@gmail.com>
2026-04-17 15:55:53 +0530

    Update monthly-performance.js

--- monthly-performance.js
+++ monthly-performance.js
@@ -4,7 +4,7 @@ import {
     updateDoc,
     serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
-import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { setDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 let academicConfig = null;
 
